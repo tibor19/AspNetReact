102824782927b79b7504bfb3ddff57df51595959
Tiberiu Covaci <tiberiu.covaci@sky.uk>
2016-02-12 14:46:30 +0000

    First version

---
+++ src/AspNetReact/webpack.config.js
@@ -0,0 +1,41 @@
+﻿var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: 'wwwroot'
+    },
+    entry: [
+        'babel-polyfill',
+        './src/main.jsx'
+    ],
+    output: {
+        path: 'wwwroot',
+        filename: 'app.[hash].js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '/wwwroot/index.html'),
+            inject: 'body'
+        }),
+        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin()
+    ],
+    module: {
+        loaders: [
+          {
+              loader: 'babel-loader',
+              test: /\.jsx?$/,
+              include: path.join(__dirname, 'src'),
+              query: {
+                  plugins: ['transform-runtime'],
+                  presets: ['es2015', 'stage-0', 'react']
+              }
+          }
+        ]
+    }
+};
